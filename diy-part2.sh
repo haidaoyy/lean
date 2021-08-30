af53d56723b3ea9c856c379df25ceaa8bfaeffa5
haidaoyy <44116974+haidaoyy@users.noreply.github.com>
2021-08-30 14:27:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,10 @@
 # Modify default IP
 # sed -i 's/5.4/5.10/g' target/linux/ath79/Makefile
 
-sed -i 's/PKG_VERSION:=1.2021.24/PKG_VERSION:=8.2021.1/g' feeds/kenzok/smartdns/Makefile
-
-sed -i 's/PKG_SOURCE_VERSION:=11cf7b614c2ad42acf12cf4843daa1b67696763b/PKG_SOURCE_VERSION:=756029f5e9879075c042030bd3aa3db06d700270/g' feeds/kenzok/smartdns/Makefile
-
 sed -i '24d' feeds/kenzok/smartdns/Makefile
+
+sed -i 's/1.2021.35/28.2021.08/g' feeds/kenzok/smartdns/Makefile
+
+sed -i 's/11cf7b614c2ad42acf12cf4843daa1b67696763b/f50e4dd0813da9300580f7188e44ed72a27ae79c/g' feeds/kenzok/smartdns/Makefile
+
+
